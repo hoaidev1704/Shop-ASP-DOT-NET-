a36c5396dfbdd34add5e29ffa8953195eb34739c
VuuTruongNhatThanhh <116423300+VuuTruongNhatThanhh@users.noreply.github.com>
2022-12-02 22:13:32 +0700

    Add project files.

---
+++ RauCuQuaShop/Areas/Admin/Controllers/HomeeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RauCuQuaShop.Areas.Admin.Controllers
+{
+    public class HomeeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult addProduct()
+        {
+            return View();
+        }
+        public ActionResult checkout()
+        {
+            return View();
+        }
+        public ActionResult user()
+        {
+            return View();
+        }
+        public ActionResult addUser()
+        {
+            return View();
+        }
+      
+    }
+}
---
+++ RauCuQuaShop/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RauCuQuaShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult login()
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+          
+
+            return View();
+        }
+        public ActionResult product1()
+        {
+
+
+            return View();
+        }
+        public ActionResult product2()
+        {
+
+
+            return View();
+        }
+        public ActionResult product3()
+        {
+
+
+            return View();
+        }
+        public ActionResult news()
+        {
+
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+
+
+            return View();
+        }
+        public ActionResult productDetail()
+        {
+
+
+            return View();
+        }
+        public ActionResult newsdetail()
+        {
+
+
+            return View();
+        }
+        public ActionResult cart()
+        {
+
+
+            return View();
+        }
+        public ActionResult checkout()
+        {
+
+
+            return View();
+        }
+        public ActionResult forgetpass()
+        {
+
+
+            return View();
+        }
+           public ActionResult userprofile()
+        {
+
+
+            return View();
+        }
+        public ActionResult typeproduct()
+        {
+
+
+            return View();
+        }
+    }
+}
